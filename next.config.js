30d4f7a67ceef4a8a953fe438d4220b80108a77a
bilgisen <hbkarabey@gmail.com>
2025-10-25 01:43:25 +0300

    db-3

--- next.config.js
+++ next.config.js
@@ -5,18 +5,6 @@ const nextConfig = {
   serverExternalPackages: ['@netlify/neon'],
   // Fix for module resolution in app directory
   transpilePackages: [],
-  // Turbopack configuration for Next.js 16
-  turbopack: {},
-  // Configure path aliases
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'utils': require('path').resolve(__dirname, 'utils.js'),
-      'components': require('path').resolve(__dirname, 'components'),
-      'lib': require('path').resolve(__dirname, 'lib'),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
